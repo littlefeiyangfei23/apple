bc18c1eaeed228dd52a28b64caedfb14fd685701
stargazor <350222336@qq.com>
2017-11-25 15:25:59 +0800

    int i

--- src/main/java/com/yf/apple/Student.java
+++ src/main/java/com/yf/apple/Student.java
@@ -2,6 +2,15 @@ package com.yf.apple;
 
 public class Student {
     private String name;
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
 
     public String getName() {
         return name;
